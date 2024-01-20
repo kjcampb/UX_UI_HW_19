7e41db9fab4864d7e8a9f324ff93fb8714ea7017
kjcampb <kjcampb1@gmail.com>
2024-01-19 18:23:09 -0800

    Portfolio

---
+++ js/index.js
@@ -0,0 +1,32 @@
+/*$(document).ready(function() {
+    // Fade in the landing section
+    $("#myButton").fadeIn(2000);
+    console.log('it worked')
+});*/
+
+$(document).ready(function(){
+    $(".bigPic").animate({
+      //left: '100vw',
+      opacity: 100
+    });
+  });
+
+/*var insta = document.getElementById('INST');
+
+insta.addEventListener('hover', function(){
+    img.src = '.images/Instagram Yellow.png';
+});*/
+
+
+/*var insta = document.getElementById('INST');
+
+function changeImage(insta,image) {
+if (x==1){
+    image.src = '.images/Instagram Yellow.png';
+}
+if (x == 2) {
+    image.src = '.images/Instagram.png';
+}
+}*/
+
+
