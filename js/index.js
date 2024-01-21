fd19a260c400c68ec885b26f7648edc6a0bda821
kjcampb <kjcampb1@gmail.com>
2024-01-21 15:33:43 -0800

    Added Fade-in animation
    Still cant get slide-in animation to work

--- js/index.js
+++ js/index.js
@@ -4,11 +4,41 @@
     console.log('it worked')
 });*/
 
-$(document).ready(function(){
+/*$(document).ready(function(){
     $(".bigPic").animate({
       //left: '100vw',
       opacity: 100
+    }); 
+  });*/
+
+  // With the element initially hidden, we can show it slowly:
+//$( "#clickme" ).on( "click", function() {
+    //$( "#book" ).fadeIn( "slow", function() {
+      // Animation complete
+    //});
+  //});
+
+/*$(document).ready(function(){
+    $(".bigPic").fadeIn("fast", function() {
+        opacity: 100;
     });
+});*/
+
+/*$(".bigPic").on( "click", function(){
+    $( ".bigPic" ).fadeIn( "slow", function() {
+
+    });
+});*/
+
+/*$( document.body ).on( "click", function() {
+    $( "div:hidden" ).first().fadeIn( "slow" );
+  });*/
+
+let myHero = document.getElementById('bigPic');
+
+  $(document).ready(function(){
+    myHero.classList.toggle('fade');
+    myHero.classList.toggle('move');
   });
 
 /*var insta = document.getElementById('INST');
